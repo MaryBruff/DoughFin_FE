9a80f7c7ad2608ad405c8feeb1d224f15c1bf038
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-02-04 12:41:16 -0500

    Add dollar sign to amounts in transactions table

--- src/components/TransactionsTable/TransactionsTable.js
+++ src/components/TransactionsTable/TransactionsTable.js
@@ -1,44 +1,46 @@
-import React, {useState} from 'react'
-import './TransactionsTable.css'
-import SearchBarButton from '../../assets/icons/searchbar-button.svg'
+import React, { useState } from "react";
+import "./TransactionsTable.css";
+import SearchBarButton from "../../assets/icons/searchbar-button.svg";
 
-
-const TransactionsTable = ({income, expenses}) => {
-  const incomeTransactions = income.map(transaction => {
-     return(
-      <tr className='transactions-tr'>
+const TransactionsTable = ({ income, expenses }) => {
+  const incomeTransactions = income.map((transaction) => {
+    return (
+      <tr className="transactions-tr">
         <td>{transaction.source}</td>
         <td>{transaction.date}</td>
-        <td>{transaction.amount}</td>
-        <td className='transactions-status-text'>Deposited</td>
+        <td>${transaction.amount}</td>
+        <td className="transactions-status-text">Deposited</td>
       </tr>
-     )
-  })
-  const expenseTransactions = expenses.map(transaction => {
-    return(
-     <tr className='transactions-tr'>
-       <td>{transaction.description}</td>
-       <td>{transaction.date}</td>
-       <td>{transaction.amount}</td>
-      <td className='transactions-status-text'>Completed</td>
-     </tr>
-    )
- })
+    );
+  });
+  const expenseTransactions = expenses.map((transaction) => {
+    return (
+      <tr className="transactions-tr">
+        <td>{transaction.description}</td>
+        <td>{transaction.date}</td>
+        <td>${transaction.amount}</td>
+        <td className="transactions-status-text">Completed</td>
+      </tr>
+    );
+  });
   return (
-    <section className='transactions'>
-      <header className='transactions-header'>
-        <h1 className='transactions-h1'>Transactions</h1>
-        <div className='transactions-searchbar-container'>
-          <div className='searchbar-flex'>
-            <input className='searchbar-text' type='text' placeholder='Search...' />
-            <button className='searchbar-button'></button>
+    <section className="transactions">
+      <header className="transactions-header">
+        <h1 className="transactions-h1">Transactions</h1>
+        <div className="transactions-searchbar-container">
+          <div className="searchbar-flex">
+            <input
+              className="searchbar-text"
+              type="text"
+              placeholder="Search..."
+            />
+            <button className="searchbar-button"></button>
           </div>
         </div>
       </header>
       <div className="table-scroll">
-
-        <table className='transactions-table-container'>
-          <thead >
+        <table className="transactions-table-container">
+          <thead>
             <tr>
               <th>Name</th>
               <th>Date</th>
@@ -53,8 +55,7 @@ const TransactionsTable = ({income, expenses}) => {
         </table>
       </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default TransactionsTable
+export default TransactionsTable;
