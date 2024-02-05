79d1b48f15b099989051fdf0a43f5db04f298a88
Mary Bruff <marybruff5@gmail.com>
2024-02-05 16:31:08 -0700

    Add state to dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,38 +1,52 @@
-import React from 'react'
-import './Dashboard.css'
-import Total from '../Total/Total'
-import TransactionsTable from '../TransactionsTable/TransactionsTable'
-import Budget from '../Budget/Budget'
-import DownArrow from '../../assets/icons/down-arrow.svg'
-import UpArrow from '../../assets/icons/up-arrow.svg'
+import React from "react";
+import "./Dashboard.css";
+import Total from "../Total/Total";
+import TransactionsTable from "../TransactionsTable/TransactionsTable";
+import Budget from "../Budget/Budget";
 
-const Dashboard = ({income, expenses, incomeData, expensesData}) => {
+const Dashboard = ({
+  income,
+  setIncome,
+  totalIncome,
+  setTotalIncome,
+  expenses,
+  totalExpenses,
+  setTotalExpenses,
+  setExpenses,
+  incomeData,
+  expensesData,
+}) => {
   return (
-    <main className='dashboard'>
-      <section className='dashboard-section'>
-        <table className='dashboard-activity-chart'>
-          A Chart.js chart will go here
+    <main className="dashboard">
+      <section className="dashboard-section">
+        <table className="dashboard-activity-chart">
         </table>
-        <div className='dashboard-totals-container'>
-          {/* The Total component will need conditional rendering to for the arrow AND percentage */}
+        <div className="dashboard-totals-container">
           <Total
-            totalType={'Total Income:'}
-            totalAmount={incomeData.data.currentIncome.amount}
+            totalType={"Total Income:"}
+            setIncome={setIncome}
+            totalAmount={totalIncome}
+            setTotalIncome={setTotalIncome}
             totalPercentage={incomeData.data.currentIncome.pctChange.toString()}
-            arrow={UpArrow}
           />
           <Total
-            totalType={'Total Expenses:'}
-            totalAmount={expensesData.data.currentExpenses.amount}
+            totalType={"Total Expenses:"}
+            setExpenses={setExpenses}
+            totalAmount={totalExpenses}
+            setTotalExpenses={setTotalExpenses}
             totalPercentage={expensesData.data.currentExpenses.pctChange.toString()}
-            arrow={DownArrow}
           />
         </div>
-        <TransactionsTable income={income} expenses={expenses} />
+        <TransactionsTable
+          income={income}
+          setIncome={setIncome}
+          expenses={expenses}
+          setExpenses={setExpenses}
+        />
       </section>
       <Budget />
     </main>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
