c4a51e4ba3a9778af2df0da94a3d6d49f168312a
Mary Bruff <marybruff5@gmail.com>
2024-02-07 11:50:07 -0700

    Add custom useQuery hooks w/ useEffect to update states + general refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,33 +1,57 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../NavBar/NavBar'
 import Dashboard from '../Dashboard/Dashboard'
-import transactionsData from "../../TransactionsData.json"
 import cashflowData from "../../CashFlowData.json"
-import incomeData from "../../IncomeData.json"
-import expensesData from "../../ExpensesData.json"
+// import transactionsData from "../sample-data/TransactionsData.json"
+// import incomeData from "../sample-data/IncomeData.json"
+// import expensesData from "../sample-data/ExpensesData.json"
 import './App.css';
+import { useGetIncomes } from '../apollo-client/queries/getIncomes';
+import { useGetExpenses } from '../apollo-client/queries/getExpenses';
+import { useGetTransactions } from '../apollo-client/queries/getTransactions';
 
 
 const App = () => {
-  const [income, setIncome] = useState(transactionsData.data.income);
-  const [expenses, setExpenses] = useState(transactionsData.data.expenses);
-  const [totalIncome, setTotalIncome] = useState(incomeData.data.currentIncome.amount.toFixed(2));
-  const [totalExpenses, setTotalExpenses] = useState(expensesData.data.currentExpenses.amount.toFixed(2));
+  const [transactions, setTransactions] = useState([]);
+  const [incomeTransactions, setIncomeTransactions] = useState([]);
+  const [expensesTransactions, setExpensesTransactions] = useState([]);
+  const [totalIncome, setTotalIncome] = useState(null);
+  const [totalExpenses, setTotalExpenses] = useState(null);
+  
+  {/* Hardcoded user, will pull from getUser endpoint soon */}
   const userName = cashflowData.data.cashFlow.username;
+  const email = "moneybaggins@bigbanktakelilbank.doge"
+
+  const { loading: loadingIncomes, error: errorIncomes, totalIncomeData } = useGetIncomes(email);
+  const { loading: loadingExpenses, error: errorExpenses, totalExpensesData } = useGetExpenses(email);
+  const { loading: loadingTransactions, error: errorTransactions, transactionsData } = useGetTransactions(email);
+
+  useEffect(() => {
+    if (totalIncomeData) setTotalIncome(totalIncomeData);
+    if (totalExpensesData) setTotalExpenses(totalExpensesData);
+    if (transactionsData) setTransactions(transactionsData);
+  }, [totalIncomeData, totalExpensesData, transactionsData]);
+
+  useEffect(() => {
+    const incomeTransactions = transactions.filter(t => t.type === 'income');
+    const expenseTransactions = transactions.filter(t => t.type === 'expense');
+    setIncomeTransactions(incomeTransactions);
+    setExpensesTransactions(expenseTransactions);
+  }, [transactions]);
+
   return (
     <main className='app'>
       <NavBar userName={userName} />
       <Dashboard
-        income={income}
+        transactions={transactions}
         totalIncome={totalIncome}
         setTotalIncome={setTotalIncome}
-        setIncome={setIncome}
-        expenses={expenses}
         totalExpenses={totalExpenses}
         setTotalExpenses={setTotalExpenses}
-        setExpenses={setExpenses}
-        incomeData={incomeData}
-        expensesData={expensesData}
+        incomeTransactions={incomeTransactions}
+        setIncomeTransactions={setIncomeTransactions}
+        expensesTransactions={expensesTransactions}
+        setExpensesTransactions={setExpensesTransactions}
       />
     </main>
   )
