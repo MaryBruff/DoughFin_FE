8bed8069684cf59ef911a2c19120654cc9eef256
Mary Bruff <marybruff5@gmail.com>
2024-02-07 22:28:33 -0700

    setting state in app for cash flow and fixing credited & debited

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import NavBar from '../NavBar/NavBar'
 import Dashboard from '../Dashboard/Dashboard'
-import cashflowData from "../../CashFlowData.json"
-// import transactionsData from "../sample-data/TransactionsData.json"
+// import cashflowData from "../sample-data/CashFlowData.json"
+// import transactionsFixtureData from "../sample-data/TransactionsData.json"
 // import incomeData from "../sample-data/IncomeData.json"
 // import expensesData from "../sample-data/ExpensesData.json"
 import './App.css';
 import { useGetIncomes } from '../apollo-client/queries/getIncomes';
 import { useGetExpenses } from '../apollo-client/queries/getExpenses';
 import { useGetTransactions } from '../apollo-client/queries/getTransactions';
+import { useGetCashFlow } from '../apollo-client/queries/getCashFlow';
 
 
 const App = () => {
@@ -17,24 +18,27 @@ const App = () => {
   const [expensesTransactions, setExpensesTransactions] = useState([]);
   const [totalIncome, setTotalIncome] = useState(null);
   const [totalExpenses, setTotalExpenses] = useState(null);
-  
-  {/* Hardcoded user, will pull from getUser endpoint soon */}
-  const userName = cashflowData.data.cashFlow.username;
-  const email = "moneybaggins@bigbanktakelilbank.doge"
+  const [cashFlow, setCashFlow] = useState(null);
 
+  {/* Hardcoded user, will pull from getUser endpoint soon */}
+  const userName = "Powdered Toast Man";
+  const email = "moneybaggins@bigbanktakelilbank.doge"
+  
   const { loading: loadingIncomes, error: errorIncomes, totalIncomeData } = useGetIncomes(email);
   const { loading: loadingExpenses, error: errorExpenses, totalExpensesData } = useGetExpenses(email);
   const { loading: loadingTransactions, error: errorTransactions, transactionsData } = useGetTransactions(email);
+  const { loading: loadingCashFlow, error: errorCashFlow, cashFlowData } = useGetCashFlow(email);
 
   useEffect(() => {
     if (totalIncomeData) setTotalIncome(totalIncomeData);
     if (totalExpensesData) setTotalExpenses(totalExpensesData);
     if (transactionsData) setTransactions(transactionsData);
-  }, [totalIncomeData, totalExpensesData, transactionsData]);
+    if (cashFlowData) setCashFlow(cashFlowData);
+  }, [totalIncomeData, totalExpensesData, transactionsData, cashFlowData]);
 
   useEffect(() => {
-    const incomeTransactions = transactions.filter(t => t.type === 'income');
-    const expenseTransactions = transactions.filter(t => t.type === 'expense');
+    const incomeTransactions = transactions.filter(t => t.status === 'credited');
+    const expenseTransactions = transactions.filter(t => t.status === 'debited');
     setIncomeTransactions(incomeTransactions);
     setExpensesTransactions(expenseTransactions);
   }, [transactions]);
@@ -43,6 +47,7 @@ const App = () => {
     <main className='app'>
       <NavBar userName={userName} />
       <Dashboard
+        cashFlow={cashFlow}
         transactions={transactions}
         totalIncome={totalIncome}
         setTotalIncome={setTotalIncome}
